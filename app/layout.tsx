436d4e1211aaddb1d911bc8465567f6d28caaca7
Victoria Banjo <67071771+banjovic@users.noreply.github.com>
2023-09-13 22:58:20 +0100

    store creation...

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ModalProvider } from "@/providers/modal-provider";
+import ToasterProvider from "@/providers/toast-providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang='en'>
         <body className={inter.className}>
+          <ToasterProvider />
           <ModalProvider />
           {children}
         </body>
