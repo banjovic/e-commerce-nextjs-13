fbe5ae6785de86af5074acaa7f22e84f96934808
Victoria Banjo <67071771+banjovic@users.noreply.github.com>
2023-09-25 14:55:29 +0100

    sizes and...

---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+import { Copy, Delete, Edit, MoreHorizontal } from "lucide-react";
+import toast from "react-hot-toast";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { CategoryColumn } from "./columns";
+import { Button } from "@/components/ui/button";
+import AlertModal from "@/components/ui/modals/alert-modal";
+
+interface CellActionProps {
+  data: CategoryColumn;
+}
+
+const CellAction = ({ data }: CellActionProps) => {
+  const router = useRouter();
+  const params = useParams();
+
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success("Category Id copied to the clipboard");
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/categories/${data.id}`);
+      router.refresh();
+      toast.success("Category deleted successfully");
+    } catch (error) {
+      toast.error("Make sure to remove all products using this category first");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant='ghost'>
+            <span className='sr-only'>Open menu</span>
+            <MoreHorizontal className='h-4 w-4' />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent align='end'>
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem onClick={() => onCopy(data.id)}>
+            <Copy className='mr-2 h-4 w-4' /> Copy Id
+          </DropdownMenuItem>
+
+          <DropdownMenuItem
+            onClick={() =>
+              router.push(`/${params.storeId}/categories/${data.id}`)
+            }
+          >
+            <Edit className='mr-2 h-4 w-4' /> Update
+          </DropdownMenuItem>
+
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            <Delete className='mr-2 h-4 w-4' /> Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
+
+export default CellAction;
--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -2,48 +2,48 @@ import { auth } from "@clerk/nextjs"
 import { NextResponse } from "next/server"
 import prismadb from "@/lib/prismadb";
 
-export async function GET(req: Request, { params }: { params: { billboardId: string } }) {
+export async function GET(req: Request, { params }: { params: { categoryId: string } }) {
     try {
-        if (!params.billboardId) {
-            return new NextResponse('Billboard id is required', { status: 400 })
+        if (!params.categoryId) {
+            return new NextResponse('Category id is required', { status: 400 })
         }
 
-        const billboard = await prismadb.billBoard.findUnique({
+        const category = await prismadb.category.findUnique({
             where: {
-                id: params.billboardId,
+                id: params.categoryId,
             },
         })
 
-        return NextResponse.json(billboard)
+        return NextResponse.json(category)
 
     } catch (error) {
-        console.log('[BILLBOARD_GET]', error)
+        console.log('[CATEGORY_GET]', error)
         return new NextResponse('Internal error', { status: 500 })
     }
 
 }
 
-export async function PATCH(req: Request, { params }: { params: { storeId: string, billboardId: string } }) {
+export async function PATCH(req: Request, { params }: { params: { storeId: string, categoryId: string } }) {
     try {
         const { userId } = auth();
         const body = await req.json();
 
-        const { label, imageUrl } = body;
+        const { name, billboardId } = body;
 
         if (!userId) {
             return new NextResponse('Unauthenticated', { status: 401 })
         }
 
-        if (!label) {
+        if (!name) {
             return new NextResponse('Label is required', { status: 400 })
         }
 
-        if (!imageUrl) {
-            return new NextResponse('Image URL is required', { status: 400 })
+        if (!billboardId) {
+            return new NextResponse('Billboard ID is required', { status: 400 })
         }
 
-        if (!params.billboardId) {
-            return new NextResponse('Billboard ID is required', { status: 400 })
+        if (!params.categoryId) {
+            return new NextResponse('Category ID is required', { status: 400 })
         }
 
         const storeByUserId = await prismadb.store.findFirst({
@@ -57,25 +57,25 @@ export async function PATCH(req: Request, { params }: { params: { storeId: strin
             return new NextResponse('Unauthorized', { status: 403 })
         }
 
-        const billboard = await prismadb.billBoard.updateMany({
+        const category = await prismadb.category.updateMany({
             where: {
-                id: params.billboardId,
+                id: params.categoryId,
             },
             data: {
-                label, imageUrl
+                name, billboardId
             }
         })
 
-        return NextResponse.json(billboard)
+        return NextResponse.json(category)
 
     } catch (error) {
-        console.log('[BILLBOARD_PATCH]', error)
+        console.log('[CATEGORY_PATCH]', error)
         return new NextResponse('Internal error', { status: 500 })
     }
 
 }
 
-export async function DELETE(req: Request, { params }: { params: { storeId: string, billboardId: string } }) {
+export async function DELETE(req: Request, { params }: { params: { storeId: string, categoryId: string } }) {
     try {
         const { userId } = auth();
 
@@ -83,8 +83,8 @@ export async function DELETE(req: Request, { params }: { params: { storeId: stri
             return new NextResponse('Unauthenticated', { status: 401 })
         }
 
-        if (!params.billboardId) {
-            return new NextResponse('Billboard id is required', { status: 400 })
+        if (!params.categoryId) {
+            return new NextResponse('Category ID is required', { status: 400 })
         }
 
         const storeByUserId = await prismadb.store.findFirst({
@@ -98,16 +98,16 @@ export async function DELETE(req: Request, { params }: { params: { storeId: stri
             return new NextResponse('Unauthorized', { status: 403 })
         }
 
-        const billboard = await prismadb.billBoard.deleteMany({
+        const category = await prismadb.category.deleteMany({
             where: {
-                id: params.billboardId,
+                id: params.categoryId,
             },
         })
 
-        return NextResponse.json(billboard)
+        return NextResponse.json(category)
 
     } catch (error) {
-        console.log('[BILLBOARD_DELETE]', error)
+        console.log('[CATEGORY_DELETE]', error)
         return new NextResponse('Internal error', { status: 500 })
     }
 
